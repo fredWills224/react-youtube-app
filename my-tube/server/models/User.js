8015c1673b30a355ea40c731d75e3be994b65d54
fredWills224 <fredwills4229@gmail.com>
2020-08-14 10:19:08 -0400

    switching user

--- my-tube/server/models/User.js
+++ my-tube/server/models/User.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const moment = require("moment");
 
 const userSchema = mongoose.Schema({
+    
     name: {
         type:String,
         maxlength:50
@@ -33,36 +34,43 @@ const userSchema = mongoose.Schema({
     tokenExp :{
         type: Number
     }
-})
+    
+});
 
 
 userSchema.pre('save', function( next ) {
-    var user = this;
     
+    var user = this;
     if(user.isModified('password')){    
+        
         // console.log('password changed')
         bcrypt.genSalt(saltRounds, function(err, salt){
+            
             if(err) return next(err);
-    
             bcrypt.hash(user.password, salt, function(err, hash){
                 if(err) return next(err);
-                user.password = hash 
-                next()
-            })
-        })
+                user.password = hash; 
+                next();
+            });
+
+        });
+        
     } else {
-        next()
+        next();
     }
 });
 
 userSchema.methods.comparePassword = function(plainPassword,cb){
+    
     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
         if (err) return cb(err);
-        cb(null, isMatch)
-    })
+        cb(null, isMatch);
+    });
+
 }
 
 userSchema.methods.generateToken = function(cb) {
+    
     var user = this;
     console.log('user',user)
     console.log('userSchema', userSchema)
@@ -74,18 +82,22 @@ userSchema.methods.generateToken = function(cb) {
     user.save(function (err, user){
         if(err) return cb(err)
         cb(null, user);
-    })
+    });
+
 }
 
 userSchema.statics.findByToken = function (token, cb) {
+    
     var user = this;
-
     jwt.verify(token,'secret',function(err, decode){
+        
         user.findOne({"_id":decode, "token":token}, function(err, user){
             if(err) return cb(err);
             cb(null, user);
-        })
-    })
+        });
+
+    });
+
 }
 
 const User = mongoose.model('User', userSchema);
