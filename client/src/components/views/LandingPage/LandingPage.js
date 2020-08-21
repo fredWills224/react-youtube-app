f2a4cb71b2d7a19e0632190ff293f106b1e11035
fredWills224 <fredwills4229@gmail.com>
2020-08-21 17:41:25 -0400

    rendering cards for each video

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,24 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import { Card, Avatar, Col, Typography, Row } from 'antd';
 import axios from 'axios';
-
+import moment from 'moment';
 
 const { Title } = Typography;
 const { Meta } = Card;
 
 function LandingPage() {
     
+    const [Videos, setVideos] = useState([]);
+
     useEffect(() => {
+        
         axios.get('/api/video/getVideos')
             .then(response => {
                 
                 if(response.data.success){
-                    console.log(response.data);
+                    console.log(response.data.videos)
+                    setVideos(response.data.videos);
                 }else{
                     alert('failed to get videos');
                 }
@@ -24,42 +28,54 @@ function LandingPage() {
 
     }, [])
     
+    const renderCards = Videos.map((video, index) =>{
+        
+        let minutes = Math.floor(video.duration/60);
+        let seconds = Math.floor(video.duration - minutes * 60);
 
+        return (
+            <Col lg={6} md={8} xs={24}>
+
+                <div style={{position:'relative'}}>
+                    <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
+                    <div className="duration"
+                        style={{ bottom:0, right:0, position:'absolute', margin:'4px',
+                        color: 'fff', backgroundColer: 'rgba(17,17,17, 0.8)', opacity:0.8,
+                        padding: '2px 4px', borderRadius:'2px', letterSpacing:'0.5px', fontSize:'12px',
+                        fontWeight:'500', lineHeight:'12px' }}>
+                    <span> {minutes} : {seconds} </span>
+                    </div>
+                </div><br />
+            
+                <Meta
+                    avatar={
+                        <Avatar src={video.writer.image} />
+                    }
+                    title={video.title}
+                />
+                <span>{video.writer.name}</span> <br />
+                <span style={{ marginLeft: '3rem' }}>{video.views}</span>
+                -
+                <span> {moment(video.createdAt).format("MMM Do YYYY")} </span>
+                
+            </Col>
+        )
+    
+    });
 
     return (
-        <>
-            <div style={{ width: '85%', margin: '3rem auto' }}>
-                
-                <Title level={2}>Recommended</Title>                
-                <hr/>
-
-                <div>
-                    
-                    <div style={{position:'relative'}}>
-
-                        <img style={{ width: '100%' }} src />
-                        <div className="duration">
-                            <span>minute : seconds</span>
-                        </div>
-
-                    </div>
-                
-                    <Meta
-                        avatar={
-                            <Avatar src />
-                        }
-                        title
-                    />
-
-                    <span>writer name</span>
-                    <span style={{ marginLeft: '3rem' }}>views count</span>
-                    -
-                    <span>date</span>
-
-                </div>
-       
-            </div>
-        </>
+        
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            
+            <Title level={2}>Recommended</Title>                
+            <hr/>
+            
+            <Row>
+                {renderCards}
+            </Row>
+   
+        </div>
+    
     )
 }
 
