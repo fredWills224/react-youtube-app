8b5d6cf0566ef104e1ae3338a7714d625c7af2c6
fredWills224 <fredwills4229@gmail.com>
2020-08-21 18:22:16 -0400

    empty detailVideoPage

---
+++ client/src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailVideoPage() {
+    return (
+        <div>
+            DetailVideoPage            
+        </div>
+    )
+}
+
+export default DetailVideoPage;
